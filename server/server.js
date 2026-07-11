5d5bcfd6ba364684f4fb46637ad5d578d6b363bc
Sayed Ahemmad <143632368+Sayeed-5@users.noreply.github.com>
2026-07-11 11:46:49 +0530

    Project Setup
    frontend and backend project setup

---
+++ server/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend Running 🚀"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
